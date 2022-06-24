2ca3175455bbfe4ce348d70c94fa81087c7babc7
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-24 22:01:19 +0530

    [gurpreet] FindFrequencySmallParagraph

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Day19_HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///<summary>
+            ///UC1:Find the frequency of word in sentence
+            /// </summary>
+            Console.WriteLine("Welcome to Data Structure Problem Using Generics");
+            LinkedHashMap<string, int> LinkedHashMap = new LinkedHashMap<string, int>(5);
+            string sentence = "to be or not to be";
+            string[] words = sentence.ToLower().Split(" ");
+            foreach (string word in words)
+            {
+                int value = LinkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else value += 1;
+                LinkedHashMap.Add(word, value);
+            }
+            int frequency = LinkedHashMap.Get("to");
+            Console.WriteLine(frequency);
+
+
+
+        }
+    }
+}
