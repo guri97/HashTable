c59481a210eeb202a5063f232b748ad0a3ec71f5
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-24 22:06:18 +0530

    [gurpreet] GFindFrequencyLargeParagraph

--- HashTable/HashTable/Program.cs
+++ HashTable/HashTable/Program.cs
@@ -23,7 +23,24 @@
             }
             int frequency = LinkedHashMap.Get("to");
             Console.WriteLine(frequency);
+            //<UC2>
+            string Paragraph = "“Paranoids are not paranoid because they are " +
+                "paranoid but because they keep putting themselves deliberately into" +
+                " paranoid avoidable situations";
+            string[] letters = Paragraph.ToLower().Split(" ");
 
+            foreach (string word in letters)
+            {
+                int value = LinkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else value += 1;
+                LinkedHashMap.Add(word, value);
+            }
+            int frequency1 = LinkedHashMap.Get("paranoid");
+            Console.WriteLine(frequency1);
 
 
         }
